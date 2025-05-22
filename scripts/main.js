a40ec805f78763b80a21980e561bcb53438b15a7
Diego de Lima Zapora <diego.limazapora@gmail.com>
2025-05-22 17:07:15 -0300

    fas

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,15 @@
 let sas = document.getElementById("samp")
 let im = document.getElementById("img")
-let bosta = document.getElementById("fech")
+let carrinho = document.getElementById("fech")
 
 sas.addEventListener("click", () => {
     if (im.style.display === "none") {
         im.style.display = "block"
-        bosta.style.display = "block"
+        carrinho.style.display = "block"
     }
 })
 
-bosta.addEventListener("click", () => {
+carrinho.addEventListener("click", () => {
     im.style.display = "none"
-    bosta.style.display = "none"
+    carrinho.style.display = "none"
 })
